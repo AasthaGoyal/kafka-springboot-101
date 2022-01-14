27b147283149fa17f5d7ecea0fe7f305611a1cb4
Aastha <aastha.goyal@integration.works>
2022-01-14 13:28:28 +1300

    adding the missing controller annotations

--- src/main/java/com/example/controller/LibraryEventsController.java
+++ src/main/java/com/example/controller/LibraryEventsController.java
@@ -5,14 +5,18 @@ import com.example.domain.LibraryEvent;
 import com.example.domain.LibraryEventType;
 import com.example.producer.LibraryEventsProducer;
 import com.fasterxml.jackson.core.JsonProcessingException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.ExecutionException;
 
+@RestController
+@Slf4j
 public class LibraryEventsController {
 
     @Autowired
